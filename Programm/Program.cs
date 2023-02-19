20fafd20a7f3a8cc1d91cd1aed100bbd91e33232
AlexeyPanshin <dispes@mail.ru>
2023-02-19 11:21:20 +0300

    начал писать программу

--- Programm/Program.cs
+++ Programm/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+
+System.Console.WriteLine("Итоговая работа Паньшина Алексея.");
+System.Console.WriteLine();
+System.Console.WriteLine("Написать программу, которая из имеющегося массива сторк \nсформирует массив строк равным 3-ём символам.");
+System.Console.WriteLine();
+
