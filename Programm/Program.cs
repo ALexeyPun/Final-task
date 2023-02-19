dfec7917324da83476fe1ac57bd71427d5147aeb
AlexeyPanshin <dispes@mail.ru>
2023-02-19 12:28:35 +0300

    закончил писать программу

--- Programm/Program.cs
+++ Programm/Program.cs
@@ -1,4 +1,79 @@
-﻿
+﻿string UserEnter(string message)
+{
+    Console.Write(message);
+
+    return Console.ReadLine();
+}
+
+void FillArray(string[] array)
+{
+    string meaning = string.Empty;
+    int counter = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        meaning = UserEnter($"Ведите значение для {i} индекса: ");
+
+        if (meaning != string.Empty)
+        {
+            array[i] = meaning;
+            counter++;
+        }
+        else
+        {
+            Console.WriteLine("Хотите завершить ввод? ");
+            System.Console.WriteLine();
+            meaning = UserEnter("\t Введите Y для подтверждения: ");
+
+            if (meaning.ToLower() == "y") break;
+            else i--;
+
+        }
+    }
+
+    Array.Resize(ref array, counter);
+}
+
+string[] RewriteArray(string[] array, int size)
+{
+    string[] resaltArray = new string[array.Length];
+    string str = string.Empty;
+    int counter = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        str = array[i];
+
+        if (str != null && str.Length == size)
+        {
+            resaltArray[counter] = array[i];
+            counter++;
+        }
+    }
+
+    Array.Resize(ref resaltArray, counter);
+    return resaltArray;
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+int GetNumber(string message)
+{
+    int userNumber = 0;
+    while (true)
+    {
+        Console.Write(message);
+        if (int.TryParse(Console.ReadLine(), out userNumber)) return userNumber;
+        Console.WriteLine("Попробуйте ещё раз!");
+    }
+}
+
 
 
 
@@ -7,3 +82,15 @@ System.Console.WriteLine();
 System.Console.WriteLine("Написать программу, которая из имеющегося массива сторк \nсформирует массив строк равным 3-ём символам.");
 System.Console.WriteLine();
 
+System.Console.WriteLine("Заполните массив: ");
+
+string[] array = new string[100];
+FillArray(array);
+System.Console.WriteLine();
+
+int countSimbols = GetNumber("Введите нужное количество элементов строк: ");
+string[] sortArray = RewriteArray(array, countSimbols);
+
+PrintArray(array);
+System.Console.WriteLine();
+PrintArray(sortArray);
